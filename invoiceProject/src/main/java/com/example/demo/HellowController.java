8536a6e052c88d936537952b76316e901cd9b7e9
systena <systena@PCK-135-119>
2018-05-03 13:35:37 +0900

    viewの作成＆表示

---
+++ invoiceProject/src/main/java/com/example/demo/HellowController.java
@@ -0,0 +1,14 @@
+package com.example.demo;                                                                                   
+                                                                                                               
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;                                                 
+                                                                                                               
+@Controller                                                                                                
+public class HellowController {                                                                                 
+	                                                                                                           
+	@RequestMapping("/hello")                                                                                  
+	public String index(@ModelAttribute SampleForm sampleForm) {
+		return "hello";                                                         
+	}
+}
